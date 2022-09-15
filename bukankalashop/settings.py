d649f68b50ed452367ad841f64a88204c7c1bd39
mansourhasani <66014095+mansourhasani@users.noreply.github.com>
2022-09-15 12:54:31 +0430

    Update settings.py

--- bukankalashop/settings.py
+++ bukankalashop/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9z%5ut*fexb=dqivi=ix5ahyy8_y8*@+wt^#g75)t3icw8*9$c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
